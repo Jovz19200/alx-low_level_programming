b3702d11322b35615c88552a2bb6405a984f3526
Johovanis N <95220604+Jovz19200@users.noreply.github.com>
2022-10-14 03:43:41 +0200

    Create 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("Size of this char: %d byte(s)\n", sizeof(char));
+	printf("Size of this int: %d byte(s)\n", sizeof(int));
+	printf("Size of this long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of this long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of this float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
