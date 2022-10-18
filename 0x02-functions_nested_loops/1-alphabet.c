465161aaab995426ded4e573791b2ef69facaac1
root <gisubizo.jovan@gmail.com>
2022-10-18 15:35:39 -0700

    alphabet lowercase commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,13 @@
 #include "main.h"
 /**
-*main - check the code
- *print_alphabet -> print the alphabets in lower case
+ *print_alphabet - print the alphabets in lower case
  *Return: 0 always
  */
 void print_alphabet(void)
 {
-  int c;
-  for(c = 'a'; c <= 'z'; c++)
-    {
-      _putchar(c);
-    }
-  _putchar('\n');
+char c;
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
 
+_putchar('\n');
 }  
