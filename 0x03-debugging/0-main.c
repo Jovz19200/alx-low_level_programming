57834f0e4a91fb77e3c89754f583c88ee5dc5e17
root <gisubizo.jovan@gmail.com>
2022-10-20 16:15:19 -0700

    checks uploaded

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *main - testing positive_negative function for integer when input is zero
+ *Return: 0
+ */
+int main(void)
+{
+        int i = 0;
+        positive_or_negative(i);
+	return (0);
+}	
