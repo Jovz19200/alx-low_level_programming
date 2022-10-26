8e8ce45ba9908a7a5db5e3393bfb3ac926fa46e1
root <gisubizo.jovan@gmail.com>
2022-10-25 17:27:23 -0700

    reset to 98 commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *reset_to_98() - reseting the input value to 98
+ *@n: the previous value of n as a pointer.
+ *Return: void
+ */
+int void reset_to_98(int *n)
+{
+*n = 98;
+} 
