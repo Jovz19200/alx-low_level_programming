cd3d68d535316f60045f0c80101974fe15626388
root <gisubizo.jovan@gmail.com>
2022-10-27 07:24:10 -0700

    string copying

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#idfndef _MAIN_H_
+#define _MAIN_H_
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -6,3 +9,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+int _putchar(char);
+
+#endif
