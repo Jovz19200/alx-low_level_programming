ceb4362cab32d64d9ac940019ba7bb037a865592
Johovanis N <95220604+Jovz19200@users.noreply.github.com>
2022-10-27 16:38:50 +0200

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,16 +6,15 @@
  *@n: space(amount of bytes) associated
  *Return: a pointer to the dest
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-  int i;
+        int i;
   
-  for (i=0; i<n && src[i] != '/0'; i++)
-    {
-      dest[i] = src[i];
-      for (;i<n; i++)
-	dest[i] = '/0';
+        for (i=0; i<n && src[i] != '/0'; i++)
+                dest[i] = src[i];
+        for (;i<n; i++)
+	        dest[i] = '/0';
       
       return (dest);
-    }
 }
