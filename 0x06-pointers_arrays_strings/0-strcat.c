f7b132c0d8c955c122336c386479778c721e6e2c
root <gisubizo.jovan@gmail.com>
2022-10-27 03:39:26 -0700

    string concatination

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_strcat() - function should append the second argument string to the first 
+ *@dest: destination string pointer
+ *@src: source string pointer
+ *Return: changed first argument
+ */
+char *_strcat(char *dest, char *src)
+{
+  int pos1 = 0, pos2 = 0;
+  while(dest [pos1] != '/0')
+    {
+      pos1++;
+    }
+  while(src[pos2] != '/0')
+    {
+      dest[pos1] = src[pos2];
+      pos1++;
+      pos2++;
+    }
+  dest[pos1] = '/0';
+  return (dest);
+}
