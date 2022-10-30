67249890b7175d1ae5a88bc55ded8aaf0a82449e
root <gisubizo.jovan@gmail.com>
2022-10-30 16:31:11 -0700

    task 10 phix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,27 +6,21 @@
  */
 int _atoi(char *s)
 {
-	int label = 1, count = 0;
+	int label = 1;
 	unsigned int num = 0;
 
+	while (!('0' <= *s && '9' >= *s) && *s != '\0')
+	{
+		if (*s == '-')
+			label *= -1;
+		if (*s == '+')
+			label *= +1;
+		s++;
+	}
 	while ('0' <= *s && '9' >= *s && *s != '\0')
 	{
 		num = (num * 10) + (*s - '0');
 		s++;
 	}
-	while (!('0' <= *s && '9' >= *s) && *s != '\0')
-	{
-		if (*s == '-')
-		{
-			count++;
-		}
-		if (count % 2 != 0)
-			label = -1;
-		else
-			label = +1;
-		if (*s == '+')
-			label *= +1;
-		s++;
-	}
-	return (label * num);
+	return (num * label);
 }
