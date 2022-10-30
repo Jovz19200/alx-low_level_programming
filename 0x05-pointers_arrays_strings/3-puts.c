adec8bbd63332c8490b7d3318ff35079c4be3ea8
root <gisubizo.jovan@gmail.com>
2022-10-30 06:53:04 -0700

    hot fix

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,18 @@
 #include "main.h"
-
 /**
  * _puts - write string followed by new line
  * @str: string
- * Return: void
+ *
+ * Return: string and new line
  */
 void _puts(char *str)
 {
-	while (*str)
-		_putchar(*str++);
+        int i = 0;
 
+        while (str[i] != '\0')
+	{
+	        _putchar(str[i]);
+	        i++;
+	}
 	_putchar('\n');
 }
