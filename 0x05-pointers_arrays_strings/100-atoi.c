04e09f73d7798f86146f517b8230d54808560bb6
root <gisubizo.jovan@gmail.com>
2022-10-30 13:08:13 -0700

    task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _atoi - convert a string into an integer
+ * @s: points to a string to convert
+ * Return: int value
+ */
+int _atoi(char *s)
+{
+	int label = 1;
+	unsigned int num = 0;
+
+	while (('0' <= *s && '9' >= *s) && *s != '\0')
+	{
+		num = (num * 10) + (*s - '\0');
+		s++;
+	}
+	while (!('0' <= *s && '9' >= *s) && *s != '\0')
+	{
+		if (*s == '-')
+			label *= -1;
+		if (*s == '+')
+			label *= -1;
+		s++;
+	}
+	return (label * num);
+}
