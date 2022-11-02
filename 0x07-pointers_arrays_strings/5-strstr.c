5f2a19c7a3516e62d88ea8c0fb2616cabc87105e
root <gisubizo.jovan@gmail.com>
2022-11-02 10:55:11 -0700

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strstr - function to find the first occurence of the string @needle in s
+ * @haystack: pointer to the string to search from
+ * @needle: pointer to the message to search for
+ *
+ * Return: the message found in string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0, j = 0;
+
+	while (haystack[i])
+	{
+		while (needle[j] && (haystack[i] == needle[0]))
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (!(needle[j]))
+		return (haystack + i);
+	}
+	return (0);
+}
