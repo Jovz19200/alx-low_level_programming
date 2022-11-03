56c4503cc0325b0ab734b8f56abc231e525edb4a
root <gisubizo.jovan@gmail.com>
2022-11-03 09:08:12 -0700

    task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - function to set a string to a pointer
+ * @s: double pointer
+ * @to:pointer to the string
+ *
+ * Return: a double pointer
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
