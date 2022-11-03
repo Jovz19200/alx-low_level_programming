d4e8198daf246e6efa4725da97ed38f02bbcf098
root <gisubizo.jovan@gmail.com>
2022-11-03 03:52:03 -0700

    task7 phix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,26 +13,9 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size; i++)
 	{
-		/**
-		 * for (j = 0; j < size; j++)
-		{
-			if (i == j || i + j == size - 1)
-			{
-				if (i == j && i + j == size - 1)
-					diag2 += (a + (i + j));
-				if (i == j)
-					diag1 += (a + (i + j));
-				else
-					diag2 += (a + (i + j));
-			}
-			else
-				continue;
-
-		}
-		*/
 		diag1 += a[i];
 		diag2 += a[size - i - 1];
 		a += size;
 	}
-	printf("%d, %d", diag1, diag2);
+	printf("%d, %d\n", diag1, diag2);
 }
