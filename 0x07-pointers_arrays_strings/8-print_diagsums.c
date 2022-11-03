50557a65a21247634d2dc78e1f735fecd4076209
root <gisubizo.jovan@gmail.com>
2022-11-03 03:35:28 -0700

    task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,26 +9,28 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, diag1 = 0, diag2 = 0;
+	int i, diag1 = 0, diag2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
+		/*for (j = 0; j < size; j++)
 		{
 			if (i == j || i + j == size - 1)
 			{
 				if (i == j && i + j == size - 1)
-					diag2 += a[i][j];
+					diag2 += (a + (i + j));
 				if (i == j)
-					diag1 += a[i][j];
+					diag1 += (a + (i + j));
 				else
-					diag2 += a[i][j];
+					diag2 += (a + (i + j));
 			}
 			else
 				continue;
-		}
+
+		}*/
+		diag1 += a[i];
+		diag2 += a[size - i - 1];
+		a += size;
 	}
-	printf("%d", diag1);
-	printf(", ");
-	printf("%d", diag2);
+	printf("%d, %d", diag1,diag2);
 }
