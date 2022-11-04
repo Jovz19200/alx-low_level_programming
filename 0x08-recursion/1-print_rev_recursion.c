17315e29aa7aabc34e491ab17366019028e1ff32
root <gisubizo.jovan@gmail.com>
2022-11-03 17:01:43 -0700

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.c"
+/**
+ * _print_rev_recursion - function to print the string in reverse
+ *  @s: a string to reverse
+ *
+ *  Return: the reversed string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
