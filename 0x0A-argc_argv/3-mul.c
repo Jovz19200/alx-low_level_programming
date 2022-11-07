6335e03909756abf33022b02d70386da25cef655
root <gisubizo.jovan@gmail.com>
2022-11-07 15:04:16 -0800

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - mian entry
+ * @argc: the input number
+ * @argv: the container to keep the arguments
+ *
+ * Return: 0(suceess)
+ */
+int main(int argc, char *argv[])
+{
+	int product = 1, i;
+
+	if (argc < 3 )
+	{
+		printf("Error");
+		return (1);	
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			product *= atoi(argv[i]);
+		}
+		printf("%d\n", product);
+	}
+
+	return (0);
+}
