3a3380c8ac7ea1f7f919832154eae070da9ca1a7
root <gisubizo.jovan@gmail.com>
2022-11-10 15:17:49 -0800

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include<stdlib.h>
+/**
+ * _calloc - returns the pointer to first byte of memory allocated to the array
+ * @nmemb: input array
+ * @size: input
+ *
+ * Return: the pointr to the memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *A;
+	unsigned int i;
+
+	A = malloc(size *  nmemb);
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	for (i = 0; i < (size * nmemb); i++)
+		A[i] = 0;
+	if (A == NULL)
+		return (NULL);
+	return (A);
+
+}
