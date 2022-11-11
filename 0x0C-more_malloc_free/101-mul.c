d7686c06da1f84c668dcabeaf85bdef87b4b49dc
root <gisubizo.jovan@gmail.com>
2022-11-11 08:10:23 -0800

    quick fix

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -20,17 +20,21 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-
-	for (i = 1; i < 3; i++)
+	else
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		for (i = 1; i < argc; i++)
 		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			printf("Error\n");
-			exit(98);
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (argv[i][j] > 57 || argv[i][j] < 48)
+				{
+					printf("Error\n");
+					exit(98);
+				}
+			}
 		}
-	}
 	product = atol(argv[1]) * atol(argv[2]);
 	printf("%lu\n", product);
+	}
 	return (0);
 }
