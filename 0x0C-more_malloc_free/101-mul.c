7fa89345003f89ae2c94cb09328e929257425f88
root <gisubizo.jovan@gmail.com>
2022-11-11 08:02:40 -0800

    hot fix

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,36 +4,33 @@
 #include <limits.h>
 
 /**
- * _mul - return a product of a and b
+ * main - return a product of a and b
  * @argc: input
  * @argv: input
  *
  * Return: product
  */
-int _mul(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int i, j;
 	unsigned long product;
 
-	if (argc == 3)
-	{
-		for (i = 1; i < 3; i++)
-		{
-			for (j = 0; argv[i][j] != '\0'; j++)
-			{
-				if (argv[i][j] > 57 || argv[i][j] < 48)
-				printf("Error\n");
-				exit(98);
-			}
-		}
-		product = atol(argv[1]) * atol(argv[2]);
-		printf("%lu\n", product);
-	}
-
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
+	for (i = 1; i < 3; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			printf("Error\n");
+			exit(98);
+		}
+	}
+	product = atol(argv[1]) * atol(argv[2]);
+	printf("%lu\n", product);
 	return (0);
 }
