0512c1bb9698aca6379637ccf325ae88df465754
Jovz19200 <gisubizo.jovan@gmail.com>
2022-12-13 07:52:53 -0800

    quick fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,10 +7,14 @@
 char *_strdup(char *str)
 {
 	char *container;
-	int a, len = strlen(str);
+	int a, len;
 
 	if (str == NULL)
 		return (NULL);
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
 
 	container = malloc(sizeof(char) * (len + 1));
 	if (container == NULL)
