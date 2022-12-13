6b066e286a15d985776f9c890fefc04cd513f35b
Jovz19200 <gisubizo.jovan@gmail.com>
2022-12-13 09:26:50 -0800

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,16 +1,21 @@
 #include "main.h"
-
+/**
+ * str_concat - returns a sring combining s1 and s2
+ * @s1: input string 1
+ * @s2: input string 2
+ * Return: produced string
+ */
 char *str_concat(char *s1, char *s2)
 {
 	unsigned int i = 0, j = 0;
-	char *cont, *returner;
+	char *cont, *ret;
 
 	cont = s1;
 	if (s1)
 		while (*cont++)
 			i++;
 	/* s1 length kept */
-	else 
+	else
 		s1 = "";
 
 	cont = s2;
@@ -18,18 +23,18 @@ char *str_concat(char *s1, char *s2)
 		while (*cont++)
 			j++;
 	/* s2 length 2 kept */
-	else 
+	else
 		s2 = "";
 
-	returner =  malloc(i + j + 1);
-	if (!returner)
+	ret = malloc(i + j + 1);
+	if (!ret)
 		return (NULL);
 
-	cont = returner;
+	cont = ret;
 	while (*s1)
 		*cont++ = *s1++;
 	while (*s2)
 		*cont++ = *s2++;
 	*cont = '\0';
-	return (returner);
+	return (ret);
 }
