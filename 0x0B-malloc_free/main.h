fec0aab362a740cf1cd8dba5d7703b5f729b507b
Jovz19200 <gisubizo.jovan@gmail.com>
2022-12-14 08:43:32 -0800

    changes on main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include<stdlib.h>
-#include<string.h>
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 char *create_array(unsigned int size, char c);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 #endif
